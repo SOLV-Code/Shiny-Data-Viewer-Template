051399c36bd435890621909f7313964f23123d33
SOLV-Code <gottfries.pestal@solv.ca>
2018-08-31 15:55:06 -0700

    further work on initial prototype - fragment

--- server.R
+++ server.R
@@ -1,64 +1,25 @@
 
-# assuming that data.file is already read in from ui.R
-
 
 function(input, output, session) {
 
 
-	# assuming that data.file is already read in from ui.R
-				
-     # this is just a specific test of the method for passing info back and forth
-
-	 
-# Plan is to pass this from ui.R (see note there)
-# but for now just read in again	 
-# read in the data and get some details for populating the GUI
-# NOTE: THIS WAY THE OBJECTS ARE ACTUALLY FUNCTIONS
-# Need to access as cu.info()[,"CU_ID"]
-#data.file <- reactive({ read.csv("../../OUTPUT/Data Inventory/SoS_DataInventory_Table.csv",stringsAsFactors=FALSE) })
-#summary.file <- reactive({ read.csv("../../OUTPUT/Data Inventory/SoS_Data_SeriesInventory.csv",stringsAsFactors=FALSE)  })
-#cu.info <- reactive({ read.csv("../DATA/SoS_Data_CU_Info.csv",stringsAsFactors=FALSE)  })
-#cu.info <- reactive({  cu.info[order(cu.info[,"CU_ID"]),]	  })
-	 
-# do it inside reactive plot below
-# How to handle this outside once?
-
-
-
-
-# -> Starting to fix as per below
-# key is: 
-#  1) reactive() creates a function,so the last element is what the function produces
-#  2) then access it in 2 steps: cu.info.tmp <- cu.info() ;  cu.info.tmp[,"FAZ"] 
-
-
-
 
 	data.file.src <- reactive({ 
-					read.csv("DATA/SoS_DataInventory_Table_FRSoxOnly.csv",stringsAsFactors=FALSE)
+					read.csv("DATA/Stock_TimeSeries.csv",stringsAsFactors=FALSE)
 					})
 	
 	
-	cu.info.src  <- reactive({ 
+	stock.info.src  <- reactive({ 
 				#read.csv("DATA/SoS_Data_CU_Info_FRSoxOnly.csv",stringsAsFactors=FALSE)
-				cu.info.tmp <- read.csv("DATA/SoS_Data_CU_Info_FrSoxOnly.csv",stringsAsFactors=FALSE)
-				cu.info.tmp <- cu.info.tmp[order(cu.info.tmp[,"CU_ID"]),]	  	
-				cu.info.tmp
+				stock.info.tmp <- read.csv("DATA/Stock_Info.csv",stringsAsFactors=FALSE)
+				stock.info.tmp <- stock.info.tmp[order(stock.info.tmp[,"StkNmS"]),]	  	
+				stock.info.tmp
 				})
 	
 	
-	summary.file.src  <- reactive({ 
-					read.csv("DATA/SoS_Data_SeriesInventory_FRSoxOnly.csv",stringsAsFactors=FALSE)				
-					})
-				
-		
-	
-	output$output.data.list <- reactive({ 
-						list(cu.info.src = 	cu.info.src(),data.file.src = data.file.src(),
-							summary.file.src = summary.file.src()  )
-						})
+			
 						
-# CU list filters
+# Stock list filters
 
 
   faz.list <- reactive({
@@ -72,16 +33,16 @@ function(input, output, session) {
   
 
 	
-    cu.list.all <- reactive({  unique(cu.info.src()[,"CU_ID"])  })
+    stock.list.all <- reactive({  unique(stock.info.src()[,"StkNmS"])  })
    
    
-	cu.list.filtered <- reactive({  
-					cu.info.tmp <- cu.info.src()
-					cu.all.tmp <- cu.list.all()
-					filter.idx <- cu.info.tmp[,"FAZ"] %in% faz.list() & cu.info.tmp[,"Area"] %in% area.list()
-					cu.filter.tmp <- cu.info.tmp[filter.idx ,"CU_ID"]
-					names(cu.filter.tmp) <- paste(cu.filter.tmp,cu.info.tmp[match(cu.filter.tmp,cu.info.tmp[,"CU_ID"]),"CU_Acro"],sep="-")
-					cu.filter.tmp
+	stock.list.filtered <- reactive({  
+					stock.info.tmp <- stock.info.src()
+					stock.all.tmp <- stock.list.all()
+					filter.idx <- stock.info.tmp[,"Watershed"] %in% watershed.list() & stock.info.tmp[,"Area"] %in% area.list()
+					stock.filter.tmp <- stock.info.tmp[filter.idx ,"StkNmS"]
+					names(stock.filter.tmp) <- stock.filter.tmp  # placholder for future extensionto have long names and short stock ID
+					stock.filter.tmp
 					})
 					
 					
@@ -89,106 +50,60 @@ function(input, output, session) {
 					
   
   # any way to merge these?
-   observeEvent(input$faz.pick, { output$Num.Filtered.CU <- renderText({ paste(length(cu.list.filtered()),"/",length(cu.list.all()), "CUs Left" )}) })
-   observeEvent(input$area.pick, { output$Num.Filtered.CU <- renderText({ paste(length(cu.list.filtered()),"/",length(cu.list.all()), "CUs Left" )}) })
+   observeEvent(input$faz.pick, { output$Num.Filtered.Stocks<- renderText({ paste(length(stock.list.filtered()),"/",length(stock.list.all()), "Stocks Left" )}) })
+   observeEvent(input$area.pick, { output$Num.Filtered.Stocks <- renderText({ paste(length(stock.list.filtered()),"/",length(stock.list.all()), "Stocks Left" )}) })
     
 
 	observe({
-    updateSelectInput(session, "cu.pick",
-      label = "CU List",
-      choices = cu.list.filtered(),
-      selected = cu.list.filtered()[1]
+    updateSelectInput(session, "stock.pick",
+      label = "Stock List",
+      choices = stock.list.filtered(),
+      selected = stock.list.filtered()[1]
     )
   }) 
 	 
-# --------------------------------------------------------------------------------------------------	 
-# FOR NOW, JUST REPEAT THE WHOLE THING FOR THE OTHER TAB
-# Worry about better solution later
-  
-	    faz.list2 <- reactive({
-    input$faz.pick2
-	})
 
-  area.list2 <- reactive({
-    input$area.pick2
-	})
-  
-	  
-	  
-	  	cu.list.filtered2 <- reactive({  
-					cu.info.tmp <- cu.info.src()
-					cu.all.tmp <- cu.list.all()
-					filter.idx <- cu.info.tmp[,"FAZ"] %in% faz.list2() & cu.info.tmp[,"Area"] %in% area.list2()
-					cu.filter.tmp <- cu.info.tmp[filter.idx ,"CU_ID"]
-					names(cu.filter.tmp) <- paste(cu.filter.tmp,cu.info.tmp[match(cu.filter.tmp,cu.info.tmp[,"CU_ID"]),"CU_Acro"],sep="-")
-					cu.filter.tmp
-					})
-	  
-	  
-   observeEvent(input$faz.pick2, { output$Num.Filtered.CU2 <- renderText({ paste(length(cu.list.filtered2()),"/",length(cu.list.all()), "CUs Left" )}) })
-   observeEvent(input$area.pick2, { output$Num.Filtered.CU2 <- renderText({ paste(length(cu.list.filtered2()),"/",length(cu.list.all()), "CUs Left" )}) })
-    
+	 
+	 
+##### MAIN PLOT 
 
-	observe({
-    updateSelectInput(session, "cu.pick2",
-      label = "CU List",
-      choices = cu.list.filtered2(),
-      selected = cu.list.filtered2()[1]
-    )
-  }) 
-	 
-	
-# --------------------------------------------------------------	
-	 
-	 
-	 
-##### VERSION 1 PLOT 
-
-     output$t2_plot_v1 <- renderPlot({
+     output$main_plot <- renderPlot({
 	
 
-	# old version
-	# GET ALL THE DATA AND EXTRACT INFO -> NEED TO FIND A WAY TO HANDLE THIS OUTSIDE THE PLOT			
-	#data.file <- read.csv("DATA/SoS_DataInventory_Table_FRSoxOnly.csv",stringsAsFactors=FALSE)
-	#summary.file <- read.csv("DATA/SoS_Data_SeriesInventory_FRSoxOnly.csv",stringsAsFactors=FALSE)
-	#cu.info <- read.csv("DATA/SoS_Data_CU_Info_FRSoxOnly.csv",stringsAsFactors=FALSE)
-	#cu.info <- cu.info[order(cu.info[,"CU_ID"]),]	 
+
 	 
-	# new version 
-	cu.info <- cu.info.src() 
+
+	stock.info <- stock.info.src() 
 	data.file <- data.file.src() 
-	summary.file <- summary.file.src() 	
 	 
 		yrs.all.range <- range(data.file[,"year"])
 		yrs.all <- yrs.all.range[1]:yrs.all.range[2]
 	 
 	 
-	# this is the list of CUs to display 
-	cu.list <- input$cu.pick
+	# this is the stock to display 
+	stock.plot <- input$stock.pick
 		
-# with reactive above -> not working
-#info.idx <- cu.info()[,"CU_ID"]%in%cu.list
-#cu.names <- cu.info()[info.idx ,"Conservation_Unit"]
-#cu.species <- cu.info()[info.idx ,"Species_Short"]
 
-	info.idx <- cu.info[,"CU_ID"]%in%cu.list
-	cu.names <- cu.info[info.idx ,"Conservation_Unit"]
-	cu.species <- cu.info[info.idx ,"Species_Short"]		
+
+	stock.idx <- stock.info[,"StkNmS"] == stock.plot
+	stock.name <- stock.info[stock.idx ,"StkNmL"]
 		
-	num.cu <- length(cu.list) 
+	num.stocks <- length(cu.list) 
 
 
 	series.mat.template <- matrix(NA, ncol=num.cu,nrow=length(yrs.all),
 			dimnames=list(yrs.all,cu.list))
 		
 		
-	if(num.cu==0){ 
+	if(num.stocks==0){ 
 			par(mfrow=c(2,2))
 			plot(1:5,1:5,type="n", axes=FALSE, xlab="",ylab="",bty="o")
-			text(2.5,2.5,labels="No CUs selected",cex=3, col = "red")
+			text(2.5,2.5,labels="No Stocks selected",cex=3, col = "red")
 			}
 	
-	if(num.cu > 0){ 
+	if(num.stocks > 0){ 
+		
+		
 		if(num.cu<=19){pch.list <- c(1:num.cu)}
 			if(num.cu>19){ pch.list <- rep(c(1:19),5)[1:num.cu]}
 			if(num.cu>19*5){ pch.list <- rep(c(1:19),10)[1:num.cu] } # assume that plots would never have > 190 lines
@@ -239,180 +154,10 @@ function(input, output, session) {
 		  		lines(yrs.all,series1.mat[,i],pch=pch.list[i],col=col.list[i],type="o",lwd=2,cex=1.5)
 				}
 		
-		
-		
-		# -------------------------------------------------------------------------------
-		# Plot 2: Variable 2
-		
-		series2.mat <- series.mat.template
-	
-		# loop through CUs (need to get all the data first, to get axis limits
-		for(cu.use in cu.list){
-			data.file.idx <- data.file[,"ID"] == cu.use & data.file[,"Var"] == input$var2.pick
-			cu.yrs <- data.file[data.file.idx,"year"]
-			series2.mat[as.character(cu.yrs),cu.use] <- data.file[data.file.idx,"value"]
-			}
-		
-		if(input$log.plot){
-					any.neg <- sum(series2.mat < 0,na.rm=TRUE  ) > 0
-					if(!any.neg){series2.mat <- log(series2.mat); title.suffix <- "- Log"}
-					if(any.neg){series2.mat <- series2.mat ; title.suffix <- "" }
-					}
-		if(!input$log.plot){ title.suffix <- "" }
-		
-		
-		ylim2 <- range(0,series2.mat,na.rm=TRUE)
-		if(!is.finite(ylim2[2])){ylim2[2] <- 1}			
-		plot(1:5,1:5,main=paste(input$var2.pick,title.suffix,sep=""),xlab="Year",xlim=input$yr.range,ylab = input$var2.pick,ylim=ylim2, type="n",bty="n",cex.axis=1.8,cex.lab = 1.8, cex.main=2,col.main="darkblue")
-		abline(h=0,col="lightgrey",lty=2)
-		for(i in 1:length(cu.list)){
-		  		lines(yrs.all,series2.mat[,i],pch=pch.list[i],col=col.list[i],type="o",lwd=2,cex=1.5)
-				}
+		} # end if have a stock selected
 			
-			
-			
-		# -------------------------------------------------------------------------------
-		# Plot 3: Variable 3
+		})	
 		
-		series3.mat <- series.mat.template
-	
-		# loop through CUs (need to get all the data first, to get axis limits
-		for(cu.use in cu.list){
-			data.file.idx <- data.file[,"ID"] == cu.use & data.file[,"Var"] == input$var3.pick
-			cu.yrs <- data.file[data.file.idx,"year"]
-			series3.mat[as.character(cu.yrs),cu.use] <- data.file[data.file.idx,"value"]
-			}
-		
-		if(input$log.plot){
-					any.neg <- sum(series3.mat < 0 ,na.rm=TRUE ) > 0
-					if(!any.neg){series3.mat <- log(series3.mat); title.suffix <- "- Log"}
-					if(any.neg){series3.mat <- series3.mat ; title.suffix <- "" }
-					}
-		if(!input$log.plot){ title.suffix <- "" }
-		
-		ylim3 <- range(0,series3.mat,na.rm=TRUE)
-		if(!is.finite(ylim3[2])){ylim3[2] <- 1}			
-		plot(1:5,1:5,main=paste(input$var3.pick,title.suffix,sep=""),xlab="Year",xlim=input$yr.range,ylab = input$var3.pick,ylim=ylim3, type="n",bty="n",cex.axis=1.8,cex.lab = 1.8, cex.main=2,col.main="darkblue")
-		abline(h=0,col="lightgrey",lty=2)
-		for(i in 1:length(cu.list)){
-		  		lines(yrs.all,series3.mat[,i],pch=pch.list[i],col=col.list[i],type="o",lwd=2,cex=1.5)
-				}	
-			
-			
-			
-			
-		# Plot 4: Map	
-		plot(1:5,1:5,type="n", axes=FALSE, xlab="",ylab="",bty="o")
-		text(2.5,2.5,labels="Placeholder for Map",cex=3)
-		legend("topleft",legend=paste(cu.species,cu.names,sep="-"),pch=pch.list,lty=1,col=col.list,cex=2,bty="n",pt.cex=2,pt.lwd=2)
-		
-	  } # end if num.cu > 0	
-		
-		
-      }) # end 	t2_plot_v1
-
-
-	  
-##### VERSION 2 PLOT 
-
-     output$t2_plot_v2 <- renderPlot({
-				
-	# GET ALL THE DATA AND EXTRACT INFO 		
-	# new version 
-	cu.info <- cu.info.src() 
-	data.file <- data.file.src() 
-	summary.file <- summary.file.src() 	
-	 
-		yrs.all.range <- range(data.file[,"year"])
-		yrs.all <- yrs.all.range[1]:yrs.all.range[2]
-	 
-	cu.list <- input$cu.pick2
-		
-# with reactive above -> not working
-#info.idx <- cu.info()[,"CU_ID"]%in%cu.list
-#cu.names <- cu.info()[info.idx ,"Conservation_Unit"]
-#cu.species <- cu.info()[info.idx ,"Species_Short"]
-
-	info.idx <- cu.info[,"CU_ID"]%in%cu.list
-	cu.names <- cu.info[info.idx ,"Conservation_Unit"]
-	cu.species <- cu.info[info.idx ,"Species_Short"]		
-		
-	num.cu <- length(cu.list) 
-
-
-	series.mat.template <- matrix(NA, ncol=num.cu,nrow=length(yrs.all),
-			dimnames=list(yrs.all,cu.list))
-		
-		
-		
-
-		
-		if(num.cu<=19){pch.list <- c(1:num.cu)}
-			if(num.cu>19){ pch.list <- rep(c(1:19),5)[1:num.cu]}
-			if(num.cu>19*5){ pch.list <- rep(c(1:19),10)[1:num.cu] } # assume that plots would never have > 190 lines
-		
-		#if(!input$pts.plot2){pch.list[] <- NA}
-		
-		# use this for color gradient
-        #colfunc <- colorRampPalette(c("orange","gold")) # NOT WORKING, SO FOR NOW JUST RECYCLE THE FOLLOWING COLORS
-		col.list <- c("firebrick1","darkblue","gold","grey")#, colfunc(num.cu-3))
-		
-		
-				
-		#data.sub <- data.file
-		
-		
-		if(input$log.plot2){title.suffix <- "- Log"}
-		if(!input$log.plot2){title.suffix <- ""}
-		
-		
-		series1.mat <- series.mat.template
-
-		# loop through CUs (need to get all the data first, to get axis limits
-		for(cu.use in cu.list){
-			data.file.idx <- data.file[,"ID"] == cu.use & data.file[,"Var"] == input$var1.pick2
-			cu.yrs <- data.file[data.file.idx,"year"]
-			series1.mat[as.character(cu.yrs),cu.use] <- data.file[data.file.idx,"value"]
-			}
-		
-		if(input$log.plot2){series1.mat <- log(series1.mat)}
-		
-		ylim1 <- range(0,series1.mat,na.rm=TRUE)
-		
-		
-		
-		series2.mat <- series.mat.template
-	
-		# loop through CUs (need to get all the data first, to get axis limits
-		for(cu.use in cu.list){
-			data.file.idx <- data.file[,"ID"] == cu.use & data.file[,"Var"] == input$var2.pick2
-			cu.yrs <- data.file[data.file.idx,"year"]
-			series2.mat[as.character(cu.yrs),cu.use] <- data.file[data.file.idx,"value"]
-			}
-		
-		if(input$log.plot2){series2.mat <- log(series2.mat)}
-		
-		ylim2 <- range(0,series2.mat,na.rm=TRUE)
-		if(!is.finite(ylim2[2])){ylim2[2] <- 1}			
-		
-
-
-		par(pty="s")
-		
-		
-		plot(1:5,1:5,xlim=ylim1,ylim=ylim2,bty="n",xlab= input$var1.pick2,ylab=input$var2.pick2, type="n",bty="n",cex.axis=1.8,cex.lab = 1.8)
-
-		#write.csv(series1.mat,file="test1.csv")
-		#write.csv(series2.mat,file="test2.csv")
-		
-		for(i in 1:length(cu.list)){
-		  		points(series1.mat[,i],series2.mat[,i],pch=pch.list[i],col=col.list[i],lwd=2,cex=1.5)
-				}
-			
-		#plot(1:5,1:5,type="n", axes=FALSE, xlab="",ylab="",bty="o")
-		legend("topleft",legend=paste(cu.species,cu.names,sep="-"),pch=pch.list,lty=1,col=col.list,cex=1.5,bty="n",pt.cex=1.5,pt.lwd=1.5,xpd=NA)
-		
-      }) # end 	t2_plot_v2
 	  
 
 
--- ui.R
+++ ui.R
@@ -26,7 +26,7 @@ names(stock.list) <- stock.info[,"StkNmS"]
 
 
 area.list <- sort(unique(cu.info1[,"Area"]))
-watershed.list <- sort(unique(cu.info1[,"FAZ"]))
+watershed.list <- sort(unique(cu.info1[,"Watershed"]))
 
 
 
@@ -36,37 +36,29 @@ ui <- navbarPage("Data Viewer Sample App",
 
 
 
- tabPanel("Task 2 - Version 1",
+ tabPanel("Main Page",
   
     
 pageWithSidebar(
-  headerPanel("Comparing CUs"),
+  headerPanel("Time Series Plot"),
     
   sidebarPanel(
 			tags$h4("Filter By Group"),
-				selectizeInput("faz.pick", "Freshwater Adaptive Zone", choices = faz.list, multiple = TRUE ,selected=faz.list),
 				selectizeInput("area.pick", "Area", choices = area.list, multiple = TRUE ,selected=area.list),
-				helpText(span(textOutput("Num.Filtered.CU"), style="color:red")),
+				selectizeInput("watershed.pick", "Watershed", choices = watershed.list, multiple = TRUE ,selected=area.list),
+				helpText(span(textOutput("Num.Filtered.Stocks"), style="color:red")),
 			tags$hr(),
-			tags$h4("Select CUs"),
-			  	selectizeInput("cu.pick", "CU", choices = cu.list, multiple = TRUE ,selected=cu.list[1]),
-			tags$h4("Plot Layout"),
-				selectizeInput("var1.pick", "Variable 1", choices = var.list, multiple = FALSE,selected=var.list[1]),
-				selectizeInput("var2.pick", "Variable 2", choices = var.list, multiple = FALSE,selected=var.list[3]),
-				selectizeInput("var3.pick", "Variable 3", choices = var.list, multiple = FALSE,selected=var.list[2]),
-			  	sliderInput("yr.range", "Display Years", sep="",min = 1940, max = 2020, value = c(1995,2017)),
-				checkboxInput("pts.plot", label="Show Points", value = FALSE),
-				checkboxInput("log.plot", label="Log Scale", value = FALSE)
+			tags$h4("Select A Stock"),
+			  	selectizeInput("stock.pick", "Stock", choices = stock.list, multiple = FALSE ,selected=stock.list[1]),
+
 		) # end sidebar
   ,
    
 
      mainPanel(
 
-		# details at https://shiny.rstudio.com/reference/shiny/latest/plotOutput.html
-	   plotOutput("t2_plot_v1", width = "100%", height = "800px")	
+	   plotOutput("main_plot", width = "100%", height = "800px")	
 	
-
 		) # end main panel
 
   
@@ -74,42 +66,9 @@ pageWithSidebar(
   
   
   
-  ) #end page with side bar for basic settings
+  ) #end page with main plot
   ),  # end end first tab panel
   
-  tabPanel("Task 2 - Version 2",
-    
-pageWithSidebar(
-  headerPanel("Comparing CUs"),
-    
-  sidebarPanel(
-			tags$h4("Filter By Group"),
-				selectizeInput("faz.pick2", "Freshwater Adaptive Zone", choices = faz.list, multiple = TRUE ,selected=faz.list),
-				selectizeInput("area.pick2", "Area", choices = area.list, multiple = TRUE ,selected=area.list),
-				helpText(span(textOutput("Num.Filtered.CU2"), style="color:red")),
-			tags$hr(),
-			tags$h4("Select CUs"),
-			  	selectizeInput("cu.pick2", "CU", choices = cu.list, multiple = TRUE ,selected=cu.list[1]),
-			tags$h4("Plot Layout"),
-				selectizeInput("var1.pick2", "Variable 1", choices = var.list, multiple = FALSE,selected=var.list[1]),
-				selectizeInput("var2.pick2", "Variable 2", choices = var.list, multiple = FALSE,selected=var.list[2]),
-				sliderInput("yr.range2", "Data Years (not linked)", sep="",min = 1940, max = 2020, value = c(1995,2017)),
-				checkboxInput("log.plot2", label="Log Scale", value = FALSE)
-		) # end sidebar
-			  
-  ,
-   
-
-     mainPanel(			
-	     
-				   plotOutput("t2_plot_v2", width = "100%", height = "600px")	
-			
-	   
-		) # end main panel
-  
-		) #end page with side bar for  data loading
-  ), # end  second tab panel
-   
    
    
    
